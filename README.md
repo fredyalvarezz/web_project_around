9a1a35d6bb7eabb70ec58b789aabccc7ac398639
Fredy Alvarez <fredy-alexander1@hotmail.com>
2025-04-20 16:04:06 -0700

    actualizacion

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Tripleten web_project_around
+Titulo: Around the EE.AA
+proyecto: https://fredyalvarezz.github.io/web_project_around/
+
+
+Descripción del proyecto: 
+Galeria de Arte, un poco de historia y fotografias de los profesionales de diferentes partes del mundo que forman parte de la comunidad de TripleTen.
+Cada uno tiene una historia única sobre el lugar del que procede.
+
+
+Tecnologias y técnicas Usadas: 
+Se incluyeron @media querys para algunas funcionalidades de la pantalla dependiendo del tamaño en pixeles y ajustes del texto.
+CSS para diseño de la página.
+HTML para estructura de la información y bloques almacenales de información.
+
+
+Autor: Fredy Alvarez
---
+++ scripts/script.js
@@ -0,0 +1,26 @@
+let butedit = document.querySelector(".profile__info-edit-button");
+let popup = document.querySelector(".popup");
+let butclosed = document.querySelector(".popup__button-closed");
+let form = document.querySelector(".popup__container");
+let inputname = document.querySelector(".popup__input-name");
+let inputabout = document.querySelector(".popup__input-about");
+let profname = document.querySelector(".profile__info-name");
+let profabout = document.querySelector(".profile__info-details");
+let butsave = document.querySelector(".popup__container-save");
+
+function openedit(){
+inputname.value=profname.textContent;
+inputabout.value=profabout.textContent;
+popup.classList.toggle("popup__opened");
+}
+butedit.addEventListener("click", openedit);
+butclosed.addEventListener("click", openedit);
+
+function savechange(e){
+  e.preventDefault();
+  profname.textContent=inputname.value;
+  profabout.textContent=inputabout.value;
+}
+
+form.addEventListener("submit", savechange);
+
