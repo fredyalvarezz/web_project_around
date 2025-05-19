4085a3ba2ee1f6b91810ae4b1ecdd2ad2b0050c1
Fredy Alvarez <fredy-alexander1@hotmail.com>
2025-05-19 14:03:37 -0700

    validacion js

--- scripts/index.js
+++ scripts/index.js
@@ -186,3 +186,19 @@ function handleSubmit(event){
 }
 addform.addEventListener("submit", handleSubmit);
 
+//***************************************************************************************************************** */
+//validaciones de inputs
+
+// habilitar la validación llamando a enableValidation()
+// pasar todas las configuraciones en la llamada
+
+import { enableValidation } from "./validate.js";
+
+enableValidation({
+  formSelector: ".popup__form",
+  inputSelector: ".popup__input",
+  submitButtonSelector: ".popup__button",
+  inactiveButtonClass: "popup__button_disabled",
+  inputErrorClass: "popup__input_type_error",
+  errorClass: "popup__error_visible"
+});
